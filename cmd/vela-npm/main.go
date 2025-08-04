3d899c94437ee989b7ff691c32322c03dfd5c950
TimHuynh <tim.huynh@target.com>
2025-08-04 10:10:24 -0500

    update to v3

--- cmd/vela-npm/main.go
+++ cmd/vela-npm/main.go
@@ -3,13 +3,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"net/mail"
 	"os"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v3"
 
 	"github.com/go-vela/vela-npm/internal/npm"
 	"github.com/go-vela/vela-npm/version"
@@ -28,157 +30,246 @@ func main() {
 	// output the version information to stdout
 	fmt.Fprintf(os.Stdout, "%s\n", string(bytes))
 
-	app := &cli.App{
+	// create new CLI application
+	// Plugin Information
+	cmd := cli.Command{
 		Name:      "vela-npm",
 		Usage:     "Vela npm plugin for publishing NodeJS packages",
-		Version:   v.Semantic(),
-		HelpName:  "vela-npm",
 		Copyright: "Copyright 2022 Target Brands, Inc. All rights reserved.",
-		Authors: []*cli.Author{
-			{
-				Name:  "Vela Admins",
-				Email: "vela@target.com",
+		Authors: []any{
+			&mail.Address{
+				Name:    "Vela Admins",
+				Address: "vela@target.com",
 			},
 		},
-		Action: run,
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "token",
-				Aliases:     []string{"t"},
-				Usage:       "auth token",
-				EnvVars:     []string{"PARAMETER_TOKEN", "PLUGIN_TOKEN", "NPM_TOKEN"},
-				FilePath:    string("/vela/parameters/npm/token,/vela/secrets/npm/token"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "username",
-				Aliases:     []string{"u"},
-				Usage:       "name of user",
-				EnvVars:     []string{"PARAMETER_USERNAME", "PLUGIN_USERNAME", "NPM_USERNAME"},
-				FilePath:    string("/vela/parameters/npm/username,/vela/secrets/npm/username,/vela/secrets/managed-auth/username"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "password",
-				Aliases:     []string{"p"},
-				Usage:       "password for user",
-				EnvVars:     []string{"PARAMETER_PASSWORD", "PLUGIN_PASSWORD", "NPM_PASSWORD"},
-				FilePath:    string("/vela/parameters/npm/password,/vela/secrets/npm/password,/vela/secrets/managed-auth/password"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "registry",
-				Aliases:     []string{"r"},
-				Usage:       "npm registry",
-				EnvVars:     []string{"PARAMETER_REGISTRY", "PLUGIN_REGISTRY", "NPM_REGISTRY"},
-				FilePath:    string("/vela/parameters/npm/registry,/vela/secrets/npm/registry"),
-				Value:       npm.DefaultRegistry,
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "email",
-				Aliases:     []string{"e"},
-				Usage:       "email for user",
-				EnvVars:     []string{"PARAMETER_EMAIL", "PLUGIN_EMAIL", "NPM_EMAIL"},
-				FilePath:    string("/vela/parameters/npm/email,/vela/secrets/npm/email"),
-				DefaultText: "N/A",
-			},
-			&cli.BoolFlag{
-				Name:        "strict-ssl",
-				Usage:       "enables strict SSL",
-				EnvVars:     []string{"PARAMETER_STRICT_SSL", "PLUGIN_STRICT_SSL", "STRICT_SSL"},
-				FilePath:    string("/vela/parameters/npm/strict_ssl,/vela/secrets/npm/strict_ssl"),
-				DefaultText: "N/A",
-			},
-			&cli.BoolFlag{
-				Name:        "always-auth",
-				Usage:       "enables always auth",
-				EnvVars:     []string{"PARAMETER_ALWAYS_AUTH", "PLUGIN_ALWAYS_AUTH", "ALWAYS_AUTH"},
-				FilePath:    string("/vela/parameters/npm/always_auth,/vela/secrets/npm/always_auth"),
-				DefaultText: "N/A",
-			},
-			&cli.BoolFlag{
-				Name:        "skip-ping",
-				Usage:       "skips auth ping",
-				Value:       false,
-				EnvVars:     []string{"PARAMETER_SKIP_PING", "PLUGIN_SKIP_PING", "SKIP_PING"},
-				FilePath:    string("/vela/parameters/npm/skip_ping,/vela/secrets/npm/skip_ping"),
-				DefaultText: "N/A",
-			},
-			&cli.BoolFlag{
-				Name:        "first-publish",
-				Usage:       "(DEPRECATED): skips version lookup and verification for first time publishes",
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "log-level",
-				Usage:       "set log level - options: (trace|debug|info|warn|error|fatal|panic)",
-				Value:       "info",
-				EnvVars:     []string{"PARAMETER_LOG", "PARAMETER_LOG_LEVEL", "PLUGIN_LOG", "PLUGIN_LOG_LEVEL", "LOG_LEVEL", "LOG"},
-				FilePath:    string("/vela/parameters/npm/log_level,/vela/secrets/npm/log_level"),
-				DefaultText: "N/A",
-			},
-			&cli.BoolFlag{
-				Name:        "dry-run",
-				Usage:       "publish command will only do dry run",
-				Value:       false,
-				EnvVars:     []string{"PARAMETER_DRY_RUN", "PLUGIN_DRY_RUN", "DRY_RUN"},
-				FilePath:    string("/vela/parameters/npm/dry_run,/vela/secrets/npm/dry_run"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "tag",
-				Usage:       "publish package with given tag",
-				EnvVars:     []string{"PARAMETER_TAG", "PLUGIN_TAG", "TAG"},
-				FilePath:    string("/vela/parameters/npm/tag,/vela/secrets/npm/tag"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "audit-level",
-				Usage:       "The level at which an npm audit will fail - options: (none|low|moderate|high|critical)",
-				Value:       "none",
-				EnvVars:     []string{"PARAMETER_AUDIT_LEVEL", "PARAMETER_AUDIT", "PLUGIN_AUDIT_LEVEL", "PLUGIN_AUDIT", "AUDIT_LEVEL", "AUDIT"},
-				FilePath:    string("/vela/parameters/npm/audit_level,/vela/secrets/npm/audit_level"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "access",
-				Usage:       "Tells the registry whether this package should be published as public or restricted. Only applies to scoped packages, which default to restricted",
-				EnvVars:     []string{"PARAMETER_ACCESS", "PARAMETER_ACCESS", "ACCESS"},
-				FilePath:    string("/vela/parameters/npm/access,/vela/secrets/npm/access"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "ci",
-				Usage:       "set to CI environment",
-				EnvVars:     []string{"CI"},
-				FilePath:    string("/vela/parameters/npm/ci,/vela/secrets/npm/ci"),
-				DefaultText: "N/A",
-			},
-			&cli.BoolFlag{
-				Name:        "workspaces",
-				Usage:       "publish all workspaces",
-				Value:       false,
-				EnvVars:     []string{"PARAMETER_WORKSPACES", "PLUGIN_WORKSPACES", "WORKSPACES", "WS"},
-				FilePath:    string("/vela/parameters/npm/workspaces,/vela/secrets/npm/workspaces"),
-				DefaultText: "N/A",
-			},
-			&cli.StringFlag{
-				Name:        "workspace",
-				Usage:       "publish a specific workspace",
-				EnvVars:     []string{"PARAMETER_WORKSPACE", "PLUGIN_WORKSPACE", "WORKSPACE", "W"},
-				FilePath:    string("/vela/parameters/npm/workspace,/vela/secrets/npm/workspace"),
-				DefaultText: "N/A",
-			},
+		Version: v.Semantic(),
+		Action:  run,
+	}
+	cmd.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "token",
+			Aliases:     []string{"t"},
+			Usage:       "auth token",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_TOKEN"),
+				cli.EnvVar("PLUGIN_TOKEN"),
+				cli.EnvVar("NPM_TOKEN"),
+				cli.File("/vela/parameters/npm/token"),
+				cli.File("/vela/secrets/npm/token"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "username",
+			Aliases:     []string{"u"},
+			Usage:       "name of user",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_USERNAME"),
+				cli.EnvVar("PLUGIN_USERNAME"),
+				cli.EnvVar("NPM_USERNAME"),
+				cli.File("/vela/parameters/npm/username"),
+				cli.File("/vela/secrets/npm/username"),
+				cli.File("/vela/secrets/managed-auth/username"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "password",
+			Aliases:     []string{"p"},
+			Usage:       "password for user",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_PASSWORD"),
+				cli.EnvVar("PLUGIN_PASSWORD"),
+				cli.EnvVar("NPM_PASSWORD"),
+				cli.File("/vela/parameters/npm/password"),
+				cli.File("/vela/secrets/npm/password"),
+				cli.File("/vela/secrets/managed-auth/password"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "registry",
+			Aliases:     []string{"r"},
+			Usage:       "npm registry",
+			Value:       npm.DefaultRegistry,
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_REGISTRY"),
+				cli.EnvVar("PLUGIN_REGISTRY"),
+				cli.EnvVar("NPM_REGISTRY"),
+				cli.File("/vela/parameters/npm/registry"),
+				cli.File("/vela/secrets/npm/registry"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "email",
+			Aliases:     []string{"e"},
+			Usage:       "email for user",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_EMAIL"),
+				cli.EnvVar("PLUGIN_EMAIL"),
+				cli.EnvVar("NPM_EMAIL"),
+				cli.File("/vela/parameters/npm/email"),
+				cli.File("/vela/secrets/npm/email"),
+			),
+		},
+		&cli.BoolFlag{
+			Name:        "strict-ssl",
+			Usage:       "enables strict SSL",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_STRICT_SSL"),
+				cli.EnvVar("PLUGIN_STRICT_SSL"),
+				cli.EnvVar("STRICT_SSL"),
+				cli.File("/vela/parameters/npm/strict_ssl"),
+				cli.File("/vela/secrets/npm/strict_ssl"),
+			),
+		},
+		&cli.BoolFlag{
+			Name:        "always-auth",
+			Usage:       "enables always auth",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_ALWAYS_AUTH"),
+				cli.EnvVar("PLUGIN_ALWAYS_AUTH"),
+				cli.EnvVar("ALWAYS_AUTH"),
+				cli.File("/vela/parameters/npm/always_auth"),
+				cli.File("/vela/secrets/npm/always_auth"),
+			),
+		},
+		&cli.BoolFlag{
+			Name:        "skip-ping",
+			Usage:       "skips auth ping",
+			Value:       false,
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_SKIP_PING"),
+				cli.EnvVar("PLUGIN_SKIP_PING"),
+				cli.EnvVar("SKIP_PING"),
+				cli.File("/vela/parameters/npm/skip_ping"),
+				cli.File("/vela/secrets/npm/skip_ping"),
+			),
+		},
+		&cli.BoolFlag{
+			Name:        "first-publish",
+			Usage:       "(DEPRECATED): skips version lookup and verification for first time publishes",
+			DefaultText: "N/A",
+		},
+		&cli.StringFlag{
+			Name:        "log-level",
+			Usage:       "set log level - options: (trace|debug|info|warn|error|fatal|panic)",
+			Value:       "info",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_LOG"),
+				cli.EnvVar("PARAMETER_LOG_LEVEL"),
+				cli.EnvVar("PLUGIN_LOG"),
+				cli.EnvVar("PLUGIN_LOG_LEVEL"),
+				cli.EnvVar("LOG_LEVEL"),
+				cli.EnvVar("LOG"),
+				cli.File("/vela/parameters/npm/log_level"),
+				cli.File("/vela/secrets/npm/log_level"),
+			),
+		},
+		&cli.BoolFlag{
+			Name:        "dry-run",
+			Usage:       "publish command will only do dry run",
+			Value:       false,
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_DRY_RUN"),
+				cli.EnvVar("PLUGIN_DRY_RUN"),
+				cli.EnvVar("DRY_RUN"),
+				cli.File("/vela/parameters/npm/dry_run"),
+				cli.File("/vela/secrets/npm/dry_run"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "tag",
+			Usage:       "publish package with given tag",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_TAG"),
+				cli.EnvVar("PLUGIN_TAG"),
+				cli.EnvVar("TAG"),
+				cli.File("/vela/parameters/npm/tag"),
+				cli.File("/vela/secrets/npm/tag"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "audit-level",
+			Usage:       "The level at which an npm audit will fail - options: (none|low|moderate|high|critical)",
+			Value:       "none",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_AUDIT_LEVEL"),
+				cli.EnvVar("PARAMETER_AUDIT"),
+				cli.EnvVar("PLUGIN_AUDIT_LEVEL"),
+				cli.EnvVar("PLUGIN_AUDIT"),
+				cli.EnvVar("AUDIT_LEVEL"),
+				cli.EnvVar("AUDIT"),
+				cli.File("/vela/parameters/npm/audit_level"),
+				cli.File("/vela/secrets/npm/audit_level"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "access",
+			Usage:       "Tells the registry whether this package should be published as public or restricted. Only applies to scoped packages, which default to restricted",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_ACCESS"),
+				cli.EnvVar("PARAMETER_ACCESS"),
+				cli.EnvVar("ACCESS"),
+				cli.File("/vela/parameters/npm/access"),
+				cli.File("/vela/secrets/npm/access"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "ci",
+			Usage:       "set to CI environment",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("CI"),
+				cli.File("/vela/parameters/npm/ci"),
+				cli.File("/vela/secrets/npm/ci"),
+			),
+		},
+		&cli.BoolFlag{
+			Name:        "workspaces",
+			Usage:       "publish all workspaces",
+			Value:       false,
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_WORKSPACES"),
+				cli.EnvVar("PLUGIN_WORKSPACES"),
+				cli.EnvVar("WORKSPACES"),
+				cli.EnvVar("WS"),
+				cli.File("/vela/parameters/npm/workspaces"),
+				cli.File("/vela/secrets/npm/workspaces"),
+			),
+		},
+		&cli.StringFlag{
+			Name:        "workspace",
+			Usage:       "publish a specific workspace",
+			DefaultText: "N/A",
+			Sources: cli.NewValueSourceChain(
+				cli.EnvVar("PARAMETER_WORKSPACE"),
+				cli.EnvVar("PLUGIN_WORKSPACE"),
+				cli.EnvVar("WORKSPACE"),
+				cli.EnvVar("W"),
+				cli.File("/vela/parameters/npm/workspace"),
+				cli.File("/vela/secrets/npm/workspace"),
+			),
 		},
 	}
 
-	if err = app.Run(os.Args); err != nil {
+	if err = cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run(c *cli.Context) error {
+func run(_ context.Context, c *cli.Command) error {
 	// set the log level for the plugin
 	switch strings.ToLower(c.String("log-level")) {
 	case "t", "trace":
